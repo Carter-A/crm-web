7a53be3e15d32f49928d0aec7225345aa3ce841d
Carter-A <carter.s.andrew@gmail.com>
2015-11-04 09:32:07 -0500

    delete button

--- crm.rb
+++ crm.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 
 get '/' do
   @crm_app_name = "My CRM"
-  erb :index
+  erb :contacts
 end
 
 get '/contacts' do
@@ -14,7 +14,16 @@ get '/contacts/new' do
   erb :new_contact
 end
 
+get '/contacts/:id' do
+  Contact.delete_contact(params['id'].to_i)
+  redirect to('/contacts')
+end
+
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+put '/modify' do
+
 end
